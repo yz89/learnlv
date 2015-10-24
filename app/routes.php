67514665970b793339e1e4b7bd31fe33d95c452d
yzhou <yzhou@elijah.com.cn>
2015-10-25 01:15:24 +0800

    name route

--- app/routes.php
+++ app/routes.php
@@ -11,10 +11,16 @@
 |
 */
 
-Route::get('/{name?}', function($name="yzhou")
+Route::get('/username/{name?}', array('as'=>'home_route', function($name="yzhou")
 {
 	$hi = '你好';
 	$object = $name;
 	$data = array('hi'=>$hi, 'object'=>$object);
 	return View::make('home.index', $data);
+}));
+
+Route::get('/test', function(){
+	$url = URL::route('home_route');
+	dd($url);
+	return Redirect::route('home_route');
 });
